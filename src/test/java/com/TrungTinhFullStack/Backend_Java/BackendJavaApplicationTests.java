1c7e78cb828b6824fbacfeeb67e01d74c17d5f54
nguyentrungtinh <trungtinhn300@gmail.com>
2024-09-28 08:57:59 +0700

    Init project backend java

---
+++ src/test/java/com/TrungTinhFullStack/Backend_Java/BackendJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TrungTinhFullStack.Backend_Java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
